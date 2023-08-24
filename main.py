cf66adb857522404c2d342de190c95f554f8d24b
katsuyuki38 <69723848+katsuyuki38@users.noreply.github.com>
2023-08-25 07:40:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
+import time
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"Hello": "World"}
+def run_timer(background_task: BackgroundTasks, seconds: int):
+    time.sleep(seconds)
+    print(f"Timer done! Waited for {seconds} seconds.")
+
+@app.post("/start_timer/")
+async def start_timer(background_task: BackgroundTasks, seconds: int):
+    background_task.add_task(run_timer, background_task, seconds)
+    return {"message": "Timer started"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
